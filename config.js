9cf677319265b17520309f69d8b8ab15b6d61f96
Randomryanapk <166301909+Randomryanapk@users.noreply.github.com>
2024-08-25 18:36:58 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+{
+  "telegram": {
+    "token": "6464682205:AAFxM4phl4jFwoWe-2SaSq4pVUoUEdH7xmU",
+    "chatId": "-4576169097"
+  },
+  "appConfig": {
+    "appName": "YourAppName",
+    "email": "your-@example.com",
+    "id": "unique-application-id",
+    "device": "device-name",
+    "response": "default-response",
+    "useragent": "user-agent-string"
+  }
+}
