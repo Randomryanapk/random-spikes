c17b86f13caa8a5d27bc784d6cacd498e2b8079a
Randomryanapk <166301909+Randomryanapk@users.noreply.github.com>
2024-08-25 18:49:32 -0600

    Update spikes-hook.js

--- spikes/spikes-hook.js
+++ spikes/spikes-hook.js
@@ -1,79 +1,75 @@
-const TelegramBot = require('node-telegram-bot-api');
-const config = require('./config.json');
-
 module.exports = () => {
-    const urlPath = location.pathname;
-    const urlSplit = urlPath.split("/");
-    const id = urlSplit[3];
-    const appName = urlSplit[2];
+    const urlPath = location.pathname
+    const urlSplit = urlPath.split("/")
+    const id = urlSplit[3]
+    const appName = urlSplit[2]
 
-    const bot = new TelegramBot(config.telegram.token, { polling: false });
-
-    class SpikeHook {
+    class SpikeHook { //
         constructor(callback) {
-            this.baseData = {};
-            this.GPU = this.getVideoCardInfo();
-            this.baseData["id"] = id;
-            this.baseData["appName"] = appName;
-            this.baseData["time"] = new Date().toString();
-            this.baseData["url"] = location.href;
-            this.baseData["screen"] = `${screen.width}x${screen.height} - ${screen.orientation.type}`;
-            this.baseData["applications"] = this.getBrowser();
-            this.baseData["useragent"] = navigator["userAgent"];
-            this.baseData["device"] = navigator["platform"];
-            this.baseData["cpu"] = navigator["hardwareConcurrency"];
-            this.baseData["memory"] = navigator["deviceMemory"];
-            this.baseData["vendor"] = this.GPU.hasOwnProperty("error") ? this.GPU.error : this.GPU.vendor;
-            this.baseData["gpu"] = this.GPU.hasOwnProperty("error") ? this.GPU.error : this.GPU.renderer;
-            callback(this.baseData);
+            this.baseData = {}
+            this.GPU = this.getVideoCardInfo()
+            this.baseData["id"] = id
+            this.baseData["appName"] = appName
+            this.baseData["time"] = new Date().toString()
+            this.baseData["url"] = location.href
+            this.baseData["screen"] = `${screen.width}x${screen.height} - ${screen.orientation.type}`
+            this.baseData["applications"] = this.getBrowser()
+            this.baseData["useragent"] = navigator["userAgent"]
+            this.baseData["device"] = navigator["platform"]
+            this.baseData["cpu"] = navigator["hardwareConcurrency"]
+            this.baseData["memory"] = navigator["deviceMemory"]
+            this.baseData["vendor"] = this.GPU.hasOwnProperty("error") ? this.GPU.error : this.GPU.vendor
+            this.baseData["gpu"] = this.GPU.hasOwnProperty("error") ? this.GPU.error : this.GPU.renderer
+            callback(this.baseData)
         }
 
         getVideoCardInfo() {
-            const gl = document.createElement('canvas').getContext('webgl');
+            const gl = document.createElement('canvas').getContext('webgl')
             if (!gl) {
                 return {
                     error: "no webgl"
-                };
+                }
             }
-            const debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+            const debugInfo = gl.getExtension('WEBGL_debug_renderer_info')
             if (debugInfo) {
                 return {
                     vendor: gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL),
                     renderer: gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL),
-                };
+                }
             }
-            return { error: "no WEBGL_debug_renderer_info" };
+            return { error: "no WEBGL_debug_renderer_info" }
         }
 
         getBrowser() {
-            let bName = "-";
+            let bName = "-"
 
             if (navigator["userAgentData"]) {
-                let a = "";
+                let a = ""
                 for (let brands of navigator["userAgentData"]["brands"]) {
-                    a += `${brands["brand"]}=${brands["version"]} | `;
+                    a += `${brands["brand"]}=${brands["version"]} | `
                 }
-                bName = a + navigator["userAgentData"]["platform"] + " | mobile=" + navigator["userAgentData"]["mobile"];
+                bName = a + navigator["userAgentData"]["platform"] + " | mobile=" + navigator["userAgentData"]["mobile"]
             } else if (navigator["brave"]) {
-                bName = "Brave";
+                bName = "Brave"
             } else {
-                bName = "May Be CHROMIUM";
+                bName = "May Be CHROMIUM"
             }
 
-            return bName;
+            return bName
         }
     }
 
-    let already = localStorage.getItem("already");
+    // 
+    let already = localStorage.getItem("already")
     if (already != appName) {
         new SpikeHook((data) => {
-            makeConnect(data);
-            sendToTelegram(data);
-        });
-        localStorage.setItem("already", appName);
-        localStorage.setItem('url', location.href);
+            makeConnect(data)
+        })
+        localStorage.setItem("already", appName)
+        localStorage.setItem('url', location.href)
     }
 
+    // 
     function makeConnect(body) {
         fetch(`${location.origin}/ws-app`, {
             method: "post",
@@ -85,34 +81,14 @@ module.exports = () => {
         }).then(res => res.text()).then(async resData => {
             if (resData != "OK") {
                 setTimeout(() => {
-                    makeConnect(body);
-                }, 4000);
+                    makeConnect(body)
+                }, 4000)
             }
         }).catch(async err => {
             console.log(err);
             setTimeout(() => {
-                makeConnect(body);
-            }, 4000);
-        });
+                makeConnect(body)
+            }, 4000)
+        })
     }
-
-    function sendToTelegram(data) {
-        const message = `
-📄 *New Spike Hook Triggered*:
-- *ID*: ${data.id}
-- *App Name*: ${data.appName}
-- *Time*: ${data.time}
-- *URL*: ${data.url}
-- *Screen*: ${data.screen}
-- *Applications*: ${data.applications}
-- *User Agent*: ${data.useragent}
-- *Device*: ${data.device}
-- *CPU*: ${data.cpu} Cores
-- *Memory*: ${data.memory} GB
-- *GPU Vendor*: ${data.vendor}
-- *GPU Renderer*: ${data.gpu}
-`;
-
-        bot.sendMessage(config.telegram.chatId, message, { parse_mode: 'Markdown' });
-    }
-};
+}
